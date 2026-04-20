d1d18d14789e7734ae83b2a7821e2f20d3a8dff0
junaid k <junaidntd@gmail.com>
2026-04-20 16:21:31 +0530

    Time: 0 ms (100%), Space: 54.7 MB (54.89%) - LeetHub

--- 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.js
+++ 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.js
@@ -3,12 +3,16 @@
  * @return {number}
  */
 var averageValue = function(nums) {
-    let sum = 0 ; let conut = 0;
-for (let i = 0; i < nums.length; i++){
+    let sum = 0
+    let conut = 0 
+    for(i=0;i<nums.length;i++){
         if(nums[i]%2===0&&nums[i]%3===0){
             sum += nums[i]
             conut++
-      }
-    }
-     return conut ? (sum / conut) | 0 : 0;
+
+        }
+       
+        
+      
+    } return conut ? Math.floor(sum / conut) : 0;
 };
