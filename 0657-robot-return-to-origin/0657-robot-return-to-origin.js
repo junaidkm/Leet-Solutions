5219c6f963c46da1ee747971f81d4b8f2b91bc87
junaid k <junaidntd@gmail.com>
2026-05-12 15:05:08 +0530

    Time: 6 ms (62.51%), Space: 56.3 MB (51.85%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+
+    let x = 0;
+    let y = 0;
+
+    for(let i = 0; i < moves.length; i++){
+
+        if(moves[i] === "R"){
+            x++;
+        }
+
+        else if(moves[i] === "L"){
+            x--;
+        }
+
+        else if(moves[i] === "U"){
+            y++;
+        }
+
+        else if(moves[i] === "D"){
+            y--;
+        }
+    }
+
+    return x === 0 && y === 0;
+};
