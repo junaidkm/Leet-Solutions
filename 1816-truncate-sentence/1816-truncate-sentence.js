23cd426223ab4070768255de47dff58aedcd381f
junaid k <junaidntd@gmail.com>
2026-05-13 11:26:09 +0530

    Time: 1 ms (42.44%), Space: 53.7 MB (51.22%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var truncateSentence = function(s, k) {
+    let words = s.split(" ");
+    return words.slice(0, k).join(" ");
+};
