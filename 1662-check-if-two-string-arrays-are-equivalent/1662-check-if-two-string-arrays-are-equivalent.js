2b6d87201ddece0ab84e9b81eb46ab1d91f0aacb
junaid k <junaidntd@gmail.com>
2026-06-12 11:53:20 +0530

    Time: 0 ms (100%), Space: 52.9 MB (95.35%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    let newwords1 = word1.join("")
+    let newwords2 = word2.join("")
+    if(newwords1 === newwords2 ){
+        return ans = true
+    }
+    else{
+        return ans = false
+    }
+    return ans
+
+};
