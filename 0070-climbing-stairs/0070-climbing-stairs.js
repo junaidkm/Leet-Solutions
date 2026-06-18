07de1889cde5e8593076be4505a04a247aa39be6
junaid k <junaidntd@gmail.com>
2026-06-18 16:31:06 +0530

    Time: 0 ms (100%), Space: 54.2 MB (12.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -1,18 +1,14 @@
-/**
- * @param {number} n
- * @return {number}
- */
 var climbStairs = function(n) {
+    let first = 1;
+    let second = 2;
+
     if (n <= 2) return n;
 
-    let a = 1;
-    let b = 2;
-
     for (let i = 3; i <= n; i++) {
-        let c = a + b;
-        a = b;
-        b = c;
+        let temp = first + second;
+        first = second;
+        second = temp;
     }
 
-    return b;
+    return second;
 };
