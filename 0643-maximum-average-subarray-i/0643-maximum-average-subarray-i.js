3d4ba8e888373b8f5d49c67808e7f571bb514f37
junaid k <junaidntd@gmail.com>
2026-06-26 11:46:03 +0530

    Time: 1 ms (98.55%), Space: 69.4 MB (18.13%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -5,14 +5,18 @@
  */
 var findMaxAverage = function(nums, k) {
     let sum = 0;
+
     for (let i = 0; i < k; i++) {
         sum += nums[i];
     }
 
     let maxSum = sum;
+
     for (let i = k; i < nums.length; i++) {
         sum = sum - nums[i - k] + nums[i];
-        maxSum = Math.max(maxSum, sum);
+        if (sum > maxSum) {
+            maxSum = sum;
+        }
     }
 
     return maxSum / k;
