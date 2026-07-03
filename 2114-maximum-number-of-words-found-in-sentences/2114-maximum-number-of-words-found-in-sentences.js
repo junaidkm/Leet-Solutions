fb615bfe5f6407649cb0aa0d03c9b1b7bf09d1e7
junaid k <junaidntd@gmail.com>
2026-07-03 11:14:00 +0530

    Time: 3 ms (46.37%), Space: 56.4 MB (35.33%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} sentences
+ * @return {number}
+ */
+var mostWordsFound = function(sentences) {
+    let max = 0
+    let count
+    for(let i=0; i<sentences.length; i++){
+        count = 0
+        for(let j=0; j<sentences[i].length; j++){
+            if(sentences[i][j]===" ")count++
+        }
+        if(count>max)max=count
+    }return max+1
+};
