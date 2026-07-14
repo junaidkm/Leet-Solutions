02d1298ff28cf805e993cdc942b7e5e501a256c2
junaid k <junaidntd@gmail.com>
2026-07-14 09:49:42 +0530

    Time: 0 ms (100%), Space: 55.2 MB (98.23%) - LeetHub

---
+++ 3668-restore-finishing-order/3668-restore-finishing-order.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} order
+ * @param {number[]} friends
+ * @return {number[]}
+ */
+var recoverOrder = function(order, friends) {
+    let ans = [];
+
+    for (let i = 0; i < order.length; i++) {
+        if (friends.includes(order[i])) {
+            ans.push(order[i]);
+        }
+    }
+
+    return ans;
+};
