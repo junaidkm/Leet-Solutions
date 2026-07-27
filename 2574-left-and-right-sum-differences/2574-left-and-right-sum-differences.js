5a960b953443c9b889b1913b9d990645196aaf80
junaid k <junaidntd@gmail.com>
2026-07-27 10:20:25 +0530

    Time: 18 ms (7.46%), Space: 56.3 MB (94.3%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
@@ -4,19 +4,15 @@
  */
 var leftRightDifference = function(nums) {
         let answer = [];
-
     for (let i = 0; i < nums.length; i++) {
         let leftSum = 0;
         let rightSum = 0;
-
         for (let j = 0; j < i; j++) {
             leftSum += nums[j];
         }
-
         for (let j = i + 1; j < nums.length; j++) {
             rightSum += nums[j];
         }
-
         answer.push(Math.abs(leftSum - rightSum));
     }
 
