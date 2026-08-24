70303cb568f2370ac869970ef97e2f6b7512b11f
junaid k <junaidntd@gmail.com>
2026-08-24 11:23:16 +0530

    Time: 1 ms (47.18%), Space: 54 MB (81.28%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    let max = Math.max(...candies);
+let result = [];
+
+for (let i = 0; i < candies.length; i++) {
+    result.push(candies[i] + extraCandies >= max);
+}
+  return result
+};
